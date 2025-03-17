0e76552d51af93919d38b2f6338a18063927c8c8
MutumaBryan <brayantim22@gmail.com>
2025-03-17 14:19:26 +0300

    This commit is a mistake

---
+++ gamed-react/src/components/Inputs/PasswordInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function PasswordInput() {
+  return (
+    <React.Fragment>
+    <input className='px-16 py-12' type='password' placeholder='Input password'></input>
+    </React.Fragment>
+  )
+}
+
+export default PasswordInput
---
+++ gamed-react/src/components/Inputs/TextInput.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+const defaultProps = {};
+
+export default function TextInput(props) {
+  return (
+    <React.Fragment>
+      
+    </React.Fragment>
+  );
+}
+
+TextInput.propTypes = propTypes;
+TextInput.defaultProps = defaultProps;
