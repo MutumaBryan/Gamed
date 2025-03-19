ed6cc83f63ed377787429265a8199a015f162f0e
MutumaBryan <brayantim22@gmail.com>
2025-03-19 17:43:08 +0300

    feature/Slider Card components completed

--- gamed-react/src/App.js
+++ gamed-react/src/App.js
@@ -3,10 +3,14 @@ import TopLeaderBoardCard from "./Component/Card/TopLeaderBoardCard";
 // import GetStarted from './Component/Button/GetStarted';
 import EarnCard from "./Component/Card/EarnCard";
 import React from "react";
+import CreateAccount from "./Component/Button/CreateAccount";
+import SignUpGoogle from "./Component/Button/SignUpGoogle";
 
 function App() {
   return (
     <React.Fragment>
+      <SignUpGoogle />
+      <CreateAccount />
       <EarnCard />
       <TopLeaderBoardCard />
     </React.Fragment>
--- gamed-react/src/Component/Button/GetStarted.jsx
+++ gamed-react/src/Component/Button/GetStarted.jsx
@@ -16,7 +16,14 @@ const getStatrtedStyles = {
   // box-shadow: y axis x axis blur spread color | inset puts the shadow inside the item's border
 };
 
-function GetStarted() {
+function GetStarted(props) {
+  // const [color, setColor] = useState({getStatrtedStyles});
+  // const backgroundColor  = color.backgroundColor;
+
+  // function changeButtonColor() {
+
+  // }
+
   return (
     <React.Fragment>
       <button style={getStatrtedStyles}>Get Started</button>
@@ -25,46 +32,3 @@ function GetStarted() {
 }
 
 export default GetStarted;
-
-// /* Quiz Button in progress */
-
-// /* Auto layout */
-// display: flex;
-// flex-direction: row;
-// justify-content: center;
-// align-items: center;
-// padding: 12px 24px;
-// gap: 12px;
-
-// width: 161px;
-// height: 64px;
-
-// /* Sec/801 */
-// background: #61D584;
-// box-shadow: inset 2px 2px 4px rgba(236, 237, 236, 0.1), inset 20px 20px 20px -10px rgba(43, 45, 43, 0.25);
-// border-radius: 24px;
-
-// /* Inside auto layout */
-// flex: none;
-// order: 1;
-// flex-grow: 0;
-
-// /* Get Started */
-
-// width: 113px;
-// height: 24px;
-
-// font-family: 'Raleway';
-// font-style: normal;
-// font-weight: 800;
-// font-size: 20px;
-// line-height: 24px;
-// /* identical to box height, or 120% */
-// text-transform: capitalize;
-
-// color: #FEFDFF;
-
-// /* Inside auto layout */
-// flex: none;
-// order: 0;
-// flex-grow: 0;
